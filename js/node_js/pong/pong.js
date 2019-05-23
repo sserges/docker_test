c0178ae9bcdd25bcd9dff6c0706cb2c4b4388b2c
sserges <agounkeserge@gmail.com>
2019-05-23 03:20:09 -0700

    App pong updated with cache optimization

--- js/node_js/pong/pong.js
+++ js/node_js/pong/pong.js
@@ -4,6 +4,8 @@ var app = express();
 app.get('/ping', function(req, res) {
 	console.log('received');
 	res.setHeader('Content-Type', 'text/plain');
+	console.log("ping-pong");
 	res.end('PONG');
 });
 app.listen(80);
+
