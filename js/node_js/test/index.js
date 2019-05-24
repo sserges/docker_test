a1690865b3bbb49ed86913c29dd431697aa18382
sserges <agounkeserge@gmail.com>
2019-05-24 03:15:03 -0700

    Node test app updated

--- js/node_js/test/index.js
+++ js/node_js/test/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var util = require('util');
+var os = require('os');
 var app = express();
 
 app.get('/', function(req, res) {
 	res.setHeader('Content-Type', 'text/plain');
-	res.end(util.format("%s - %s", new Date(), 'Got HTTP Get Request'));
+//	res.end(util.format("%s - %s", new Date(), 'Got HTTP Get Request'));
+	res.end(util.format("hello from %s", os.hostname()));
 });
 app.listen(process.env.PORT || 80);
 
